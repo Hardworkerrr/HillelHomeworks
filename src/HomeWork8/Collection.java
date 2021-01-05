667d73807fd2e83660a48d034e8925838e2f6dea
SunnyRain224 <71274750+SunnyRain224@users.noreply.github.com>
2021-01-05 22:19:46 +0800

    Finished work

--- src/HomeWork8/Collection.java
+++ src/HomeWork8/Collection.java
@@ -1,48 +1,127 @@
 package HomeWork8;
 
-public class Collection implements NewCollection{
-    @Override
-    public boolean add(Object o) {
-        return false;
+import java.util.Arrays;
+
+public class Collection<E> implements NewCollection<E> {
+    private int elementsSize = 0;
+    private final int DEFAULT_CAPACITY = 7;
+    private int newCapacity = 0;
+    private Object[] Coll;
+
+    public Collection() {
+        Coll = new String[DEFAULT_CAPACITY];
+    }
+
+    public Collection(String str) {
+        Coll = new String[DEFAULT_CAPACITY];
+        String[] newStr = str.split(" ");
+        if (newStr.length >= DEFAULT_CAPACITY) {
+            newCapacity = (int) (newStr.length * (3.0 / 2.0) + 1);
+            Coll = new String[newCapacity];
+            Coll = Arrays.copyOf(newStr, newCapacity);
+        } else {
+            Coll = Arrays.copyOf(newStr, DEFAULT_CAPACITY);
+        }
+        elementsSize = newStr.length;
     }
 
     @Override
-    public boolean add(int index, Object o) {
-        return false;
+    public boolean add(E o) {
+        elementsSize++;
+        if (elementsSize >= newCapacity) {
+            newCapacity = (int) (Coll.length * (3.0 / 2.0) + 1);
+            Coll = Arrays.copyOf(Coll, newCapacity);
+        }
+        Coll[elementsSize - 1] = o;
+        return true;
+    }
+
+    @Override
+    public boolean add(int index, E o) {
+        elementsSize++;
+        if (elementsSize >= newCapacity) {
+            newCapacity = (int) (Coll.length * (3.0 / 2.0) + 1);
+            Coll = Arrays.copyOf(Coll, newCapacity);
+        }
+        for (int i = elementsSize - 1; i > index; i--) {
+            Coll[i] = Coll[i - 1];
+        }
+        Coll[index] = o;
+        return true;
     }
 
     @Override
     public boolean remove(int index) {
-        return false;
+        elementsSize--;
+        Coll[index] = null;
+        for (int i = index; i <= elementsSize; i++) {
+            Coll[i] = Coll[i + 1];
+        }
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
+        for (int i = 0; i <= elementsSize; i++) {
+            if (Coll[i] == o) {
+                remove(i);
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
-    public Object get(int index) {
-        return null;
+    public E get(int index) {
+        return (E) Coll[index];
     }
 
     @Override
-    public boolean contain(Object o) {
+    public boolean contain(E o) {
+        for (int i = 0; i < elementsSize; i++) {
+            if (Coll[i].equals(o)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean equals(NewCollection o) {
-        return false;
+        boolean equals = false;
+        if (this.elementsSize < o.size()) {
+            return false;
+        }
+        for (int i = 0; i < elementsSize; i++) {
+            if (Coll[i].equals(o.get(i))) {
+                equals = true;
+            } else return false;
+        }
+        return equals;
     }
 
     @Override
     public boolean clear() {
-        return false;
+        elementsSize = 0;
+        Coll = new Object[DEFAULT_CAPACITY];
+        return true;
     }
 
     @Override
     public int size() {
-        return 0;
+        return elementsSize;
+    }
+
+    @Override
+    public String toString() {
+        String str = "[";
+        for (int i = 0; i < elementsSize; i++) {
+            if (i == elementsSize - 1) {
+                str += Coll[i] + "]";
+                break;
+            }
+            str += Coll[i] + ", ";
+        }
+        return str;
     }
 }
--- src/HomeWork8/MainProgram.java
+++ src/HomeWork8/MainProgram.java
@@ -2,6 +2,27 @@ package HomeWork8;
 
 public class MainProgram {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Collection<String> collection = new Collection<>("first second third fifth sixth nine");
+        System.out.println(collection.toString());
+        collection.add("Added1");
+        System.out.println(collection.toString());
+        collection.add(1,"Added2");
+        System.out.println(collection.toString());
+        collection.remove(1);
+        System.out.println(collection.toString());
+        collection.remove("Added1");
+        System.out.println(collection.remove("Added1"));
+        System.out.println(collection.toString());
+        System.out.println(collection.get(3));
+        System.out.println(collection.contain("Dog"));
+        System.out.println(collection.contain("sixth"));
+        System.out.println("Сравнение коллекций : ");
+        Collection<String> collection2 = new Collection<>("first second third fifth sixth nine");
+        Collection<String> collection3 = new Collection<>("first second third fifth sixth eight");
+        System.out.println(collection.equals(collection2));
+        System.out.println(collection.equals(collection3));
+        collection.clear();
+
+
     }
 }
