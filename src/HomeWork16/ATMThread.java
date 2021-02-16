8898df5b762b5efb481db196786af2366f3a02c8
SunnyRain224 <71274750+SunnyRain224@users.noreply.github.com>
2021-02-16 20:18:23 +0200

    HomeWork16 Finished

--- src/HomeWork16/ATMThread.java
+++ src/HomeWork16/ATMThread.java
@@ -14,16 +14,12 @@ public class ATMThread extends Thread {
         System.out.println("Здравствуйте " + atm.getUser().getFullName() + " !");
         System.out.println("Ваш персональный номер : " + atm.getUser().getId());
         System.out.println("Количество денег на счету : " + atm.getUser().getMoneyAmount());
-        try {
-            condition();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        condition();
+
     }
 
-    public void condition() throws InterruptedException {
-        sleep(2000);
-        switch ((int)(Math.random()*3)) {
+    public void condition() {
+        switch ((int) (Math.random() * 3)) {
             case 0:
                 atm.getMoneyBalance();
                 break;
